a4453e70e3e62d0eb565e5e453a72e059eeb0157
sarthaksavvy <sarthak@bitfumes.com>
2023-06-18 12:40:48 +0530

    [main] auth done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import { createInertiaApp } from "@inertiajs/vue3";
+import { createApp, h } from "vue";
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+});
